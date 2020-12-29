a2483864e3d0459e749a4d6c0f2e3d2fb83eb628
Steven <s.hoogervorst@hotmail.com>
2020-12-29 15:29:02 +0000

    Only return data when it is correct

--- binary.py
+++ binary.py
@@ -17,7 +17,6 @@ def chunks(l, n):
 def toBinary(l):
   return int(''.join(map(str,l)),2)
 
-
 # Specify gpio pin
 port = 17
 
@@ -25,7 +24,7 @@ def read_dht11():
 
   # to use Raspberry Pi board pin numbers
   GPIO.setmode(GPIO.BCM)
- 
+
   data = [] # Define data array
 
   #### Activate sensor ####
@@ -34,7 +33,6 @@ def read_dht11():
   GPIO.setup(port, GPIO.OUT)
   # Make sure GPIO has default status of HIGH
   GPIO.output(port, GPIO.HIGH)
-
   # Set GPIO pin to LOW for 18 milliseconds to ensure DHT11 has detected the signal
   GPIO.output(port, GPIO.LOW)
   sleep(0.018)
@@ -48,22 +46,25 @@ def read_dht11():
   # prepare to read the pin
   GPIO.setup(port, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-  for i in range(0,600):
+  for i in range(0,500):
     data.append(GPIO.input(port))
 
+  # Zeroes serve as seperators
   bitsList = list(filter(None,''.join(map(str,data)).split("0")))
+
+  # Make bits of list values
   data = list(map(toBits, bitsList))[:-1]
   chunkList = list(chunks(data,8))
   binaryList = list(map(toBinary, chunkList))
-  print(binaryList)
-  print(sum(binaryList[:4])) 
 
   # If data is correct
-  if len(data) == 40 and sum(binaryList[:4]) == binaryList[4]:
-    return binaryList
-  else:
-    sleep(1)
+  if len(data) >= 40 and sum(binaryList[:4]) == binaryList[4]:
+    print(binaryList[:-1])
+#  else:
+#    sleep(1)
     #read_dht11()
     #return float(str(binaryList[0])+"."+str(binaryList[1]), float(str(binaryList[2])+"."+str(binaryList[3])
 
-read_dht11()
+while True:
+  read_dht11()
+  sleep(0.02)
